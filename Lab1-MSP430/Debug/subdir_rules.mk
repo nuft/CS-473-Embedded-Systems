16346f4a2b8bc1bc03502c2894ccb59aaf4f9b70
Michael Spieler <michael.spieler@epfl.ch>
2017-10-06 10:49:13 +0200

    ADC WIP

--- Lab1-MSP430/Debug/subdir_rules.mk
+++ Lab1-MSP430/Debug/subdir_rules.mk
@@ -6,7 +6,7 @@
 main.obj: ../main.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_15.12.1.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_15.12.1.LTS/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_15.12.1.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_15.12.1.LTS/include" --advice:power=all -g --define=__MSP430G2553__ --display_error_number --diag_warning=225 --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Lab1-MSP430/main.c
+++ Lab1-MSP430/main.c
@@ -4,6 +4,11 @@
 // setup Timer_A for ~10ms period
 void pwm_init(uint16_t duty_time_us)
 {
+    // GPIO P1.6 CCR1 output
+    P1DIR |= BIT6;
+    P1SEL |= BIT6;
+    P1SEL2 &= ~BIT6;
+
     /* TimerA0 PWM configuration:
      * input clock SMCLK = 1MHz
      * reload: TACCR0 = 10000 - 1 => 100Hz or 10ms period
@@ -23,10 +28,40 @@ void pwm_set_duty(uint16_t duty_time_us)
 }
 
 #pragma vector=TIMER0_A1_VECTOR
-__interrupt void TimerA0(void) // Interrupt routine for TAIFG
+__interrupt void TimerA0_ISR(void) // Interrupt routine for TAIFG
 {
-    P1OUT = P1OUT ^ BIT6;
+
     TA0CTL &= (~TAIFG); // Clear TAIFG flag in TA0CTL register
+}
+
+/* Configure ADC input for P1.1
+ */
+void adc_init(void)
+{
+    // P1DIR &= ~BIT5;
+    // ADC10AE0 |= BIT5;
+
+    /* Notes
+    (CONSEQ = 00)
+    ADC10ON = 1
+
+    (ENC and SAMPCON rising edge)
+
+    select channel ADC10CTL1 INCHx
+
+    ADC10DTC1 = 0
+    ADC10IE = 1
+    */
+
+    ADC10CTL0 = SREF_0 | ADC10SHT_0 | ADC10SR
+}
+
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void)
+{
+    // TODO
+    // read ADC10MEM
+    // clear ADC10IFG
 }
 
 int main(void)
@@ -44,16 +79,13 @@ int main(void)
     BCSCTL2 = SELM_3 | DIVM_0 | DIVS_0;
     BCSCTL3 = XT2S_0 | LFXT1S_0 | XCAP_1;
 
-    // P1.2 CCR1 output
-    P1DIR |= BIT2;
-    P1SEL |= BIT2;
-    P1SEL2 &= ~BIT2;
+    // P1.3 output low
+    P1DIR |= BIT3;
+    P1OUT |= BIT3;
 
     // setup PWM for 10ms period, 1ms duty time
     pwm_init(1000);
 
-    // for debugging toggle pin 6 from interrupt
-    P1DIR |= BIT6;
     // enable timer interrupt
     TA0CTL |= TAIE;
     // Enable global Interrupt
